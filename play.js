dbeb8da6ddd3e21b6093132620d6ad588c79807d
Bibek Bam <bibek2bam@gmail.com>
2023-03-01 12:43:10 +0200

    fir st commit of course

---
+++ play.js
@@ -0,0 +1,21 @@
+/* 
+let uName = 'max';
+let age = 29;
+let hasHobbies = true;
+
+const summariseUser = (userName, userAge, userHobbies) => {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ', has hobbies: ' +
+    userHobbies
+  );
+};
+console.log(summariseUser(uName, age, hasHobbies));
+*/
+
+setTimeout(() => {
+  console.log('timer is done!!!');
+}, 2000);
